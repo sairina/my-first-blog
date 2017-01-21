371cde5147857d8403d6862892dce82a6ad381b9
Sairina <sairina.merinotsui@gmail.com>
2017-01-21 15:58:31 -0500

    My Django Girls app, first commit

---
+++ blog/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.utils import timezone
+
+# does this mean that django.db exists on my computer and we're going to import from this file?
+# is timezone from settings.py
+
+class Post(models.Model):
+	author = models.ForeignKey('auth.User')
+	title = models.CharField(max_length=200)
+	text = models.TextField()
+	created_date = models.DateTimeField(
+		default=timezone.now)
+	published_date = models.DateTimeField(
+		blank=True, null=True)
+
+	def publish(self):
+		self.published_date = timezone.now()
+		self.save()
+
+	def __str__(self):
+		return self.title
